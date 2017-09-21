cbd405a3dfbd9f2a9c7c28048f9169e5831f6c61
Harry Bassett-Butt <harry.bassettbutt@insightsoftware.com>
2017-09-21 12:51:24 +0100

    Code shit

--- convert-db-to-csv.sh
+++ convert-db-to-csv.sh
@@ -14,16 +14,24 @@ then
 	exit 1
 fi
 
+if ["$2" == ""]
+then
+	echo "You must enter a valid destination path for the .csv files"
+	exit 2
+fi
+
 db=$1
+loc=$2
 
 t=($(sqlite3 $db ".tables"))
 
+# For each table in the database
 for i in "${t[@]}"
 do
 	sqlite3 $db<<- EXIT_HERE
 	.mode csv
 	.headers on
-	.output $i.csv
+	.output $2$1.csv
 	SELECT * FROM $i;
 	.exit
 	EXIT_HERE
